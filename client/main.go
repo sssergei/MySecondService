65c268d003e658b8fbb9383b4cb54ee2d3d62438
Sergey <sssergei@hotmail.com>
2021-03-31 02:19:00 +0300

    add client and server

---
+++ client/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"log"
+	"sort"
+
+	pb "https://github.com/sssergei/MySecondService/tree/main/proto"
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+var target = flag.String("l", ":7100", "Specify the port that the server is listening on")
+var username = flag.String("u", "scaramoucheX2", "Specify the username to auth the call")
+var password = flag.String("p", "Can-you-do-the-fandango?", "Specify the password to auth the call")
+
+type byVersion []*pb.ReleaseInfo
+
+func (b byVersion) Len() int           { return len(b) }
+func (b byVersion) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b byVersion) Less(i, j int) bool { return b[i].GetVersion() < b[j].GetVersion() }
+
+func main() {
+
+	flag.Parse()
+
+	// create gRPC TLS credentials
+	creds := credentials.NewTLS(&tls.Config{
+		InsecureSkipVerify: true, // using self signed certificate for demo, for more secure connections see https://bbengfort.github.io/programmer/2017/03/03/secure-grpc.html
+	})
+
+	grpcAuth := &basicAuthCreds{
+		username: *username,
+		password: *password,
+	}
+
+	conn, err := grpc.Dial(*target,
+		grpc.WithTransportCredentials(creds),
+		grpc.WithPerRPCCredentials(grpcAuth),
+	)
+	if err != nil {
+		log.Fatalf("grpc.Dial err: %v", err)
+	}
+
+	client := pb.NewGoReleaseServiceClient(conn)
+
+	ctx := context.Background()
+	rsp, err := client.ListReleases(ctx, &pb.ListReleasesRequest{})
+
+	if err != nil {
+		log.Fatalf("ListReleases err: %v", err)
+	}
+
+	releases := rsp.GetReleases()
+	if len(releases) > 0 {
+		sort.Sort(byVersion(releases))
+
+		fmt.Printf("Version\tRelease Date\tRelease Notes\n")
+	} else {
+		fmt.Println("No releases found")
+	}
+	for _, ri := range releases {
+		fmt.Printf("%s\t%s\t%s\n", ri.GetVersion(), ri.GetReleaseDate(), ri.GetReleaseNotesURL())
+	}
+}
+
+// basicAuthCreds is an implementation of credentials.PerRPCCredentials
+// that transforms the username and password into a base64 encoded value similar
+// to HTTP Basic xxx
+type basicAuthCreds struct {
+	username, password string
+}
+
+// GetRequestMetadata sets the value for "authorization" key
+func (b *basicAuthCreds) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"authorization": "Basic " + basicAuth(b.username, b.password),
+	}, nil
+}
+
+// RequireTransportSecurity should be true as even though the credentials are base64, we want to have it encrypted over the wire.
+func (b *basicAuthCreds) RequireTransportSecurity() bool {
+	return true
+}
+
+//helper function
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
---
+++ service/main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"time"
+
+	pb "https://github.com/sssergei/MySecondService/tree/main/proto"
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+var listenPort = flag.String("l", ":7100", "Specify the port that the server will listen on")
+
+type releaseInfo struct {
+	ReleaseDate     string `json:"release_date"`
+	ReleaseNotesURL string `json:"release_notes_url"`
+}
+
+/* goReleaseService implements GoReleaseServiceServer as defined in the generated code:
+// Server API for GoReleaseService service
+type GoReleaseServiceServer interface {
+	GetReleaseInfo(context.Context, *GetReleaseInfoRequest) (*ReleaseInfo, error)
+	ListReleases(context.Context, *ListReleasesRequest) (*ListReleasesResponse, error)
+}
+*/
+type goReleaseService struct {
+	releases map[string]releaseInfo
+}
+
+func (g *goReleaseService) GetReleaseInfo(ctx context.Context, r *pb.GetReleaseInfoRequest) (*pb.ReleaseInfo, error) {
+	ri, ok := g.releases[r.GetVersion()]
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "release verions %s not found", r.GetVersion())
+	}
+
+	// success
+	return &pb.ReleaseInfo{
+		Version:         r.GetVersion(),
+		ReleaseDate:     ri.ReleaseDate,
+		ReleaseNotesUrl: ri.ReleaseNotesURL,
+	}, nil
+}
+
+func (g *goReleaseService) ListReleases(ctx context.Context, r *pb.ListReleasesRequest) (*pb.ListReleasesResponse, error) {
+	var releases []*pb.ReleaseInfo
+
+	for k, v := range g.releases {
+		ri := &pb.ReleaseInfo{
+			Version:         k,
+			ReleaseDate:     v.ReleaseDate,
+			ReleaseNotesUrl: v.ReleaseNotesURL,
+		}
+
+		releases = append(releases, ri)
+	}
+
+	return &pb.ListReleasesResponse{
+		Releases: releases,
+	}, nil
+}
+
+func main() {
+	flag.Parse()
+	svc := &goReleaseService{
+		releases: make(map[string]releaseInfo),
+	}
+
+	jsonData, err := ioutil.ReadFile("../data/releases.json") // For read access.
+	if err != nil {
+		log.Fatalf("failed to read data file: %v", err)
+	}
+
+	//read releases from JSON data file
+	err = json.Unmarshal(jsonData, &svc.releases)
+	if err != nil {
+		log.Fatalf("failed to marshal release data: %v", err)
+	}
+
+	// prepare TLS Config
+	tlsCert := "../certs/demo.crt"
+	tlsKey := "../certs/demo.key"
+	cert, err := tls.LoadX509KeyPair(tlsCert, tlsKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create gRPC TLS credentials
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+	})
+
+	// create new gRPC server with Transport Credentials
+	server := grpc.NewServer(
+		grpc.Creds(creds),
+		grpc.UnaryInterceptor(unaryInterceptor),
+	)
+
+	lis, err := net.Listen("tcp", *listenPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Println("Listening on ", *listenPort)
+
+	pb.RegisterGoReleaseServiceServer(server, svc)
+
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+// general unary interceptor function
+func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	start := time.Now()
+
+	if info.FullMethod != "/proto.GoReleaseService/ListReleases" { //skip auth when ListReleases requested
+		if err := authorize(ctx); err != nil {
+			return nil, err
+		}
+	}
+
+	h, err := handler(ctx, req)
+
+	log.Printf("request - Method:%s\tDuration:%s\tError:%v\n", info.FullMethod, time.Since(start), err) //logging
+
+	return h, err
+}
+
+// authorize function that is used by the interceptor functions.
+func authorize(ctx context.Context) error {
+	// warning: this is only for illustration purposes - don't implement authorization that is hardcoded!
+	var authList = map[string]bool{
+		base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", "scaramoucheX2", "Can-you-do-the-fandango?"))): true,
+	}
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Errorf(codes.InvalidArgument, "retrieving metadata failed")
+	}
+
+	elem, ok := md["authorization"]
+	if !ok {
+		return status.Errorf(codes.InvalidArgument, "no auth details supplied")
+	}
+
+	authorization := elem[0][len("Basic "):] //extract base64 basic auth value (similar to HTTP Basic Auth)
+	valid, ok := authList[authorization]
+	if !ok {
+		return status.Errorf(codes.NotFound, "auth not found")
+	}
+
+	if !valid {
+		return status.Errorf(codes.Unauthenticated, "auth failed")
+	}
+
+	return nil
+}
